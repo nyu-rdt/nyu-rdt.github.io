772e72476969a91ad06a503b413061f7969709f0
jriv01 <jusrivera028@gmail.com>
2022-10-06 11:57:51 -0400

    Added some responsive elements

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var i = 0;
-var w = 0
-var word = 'Mission: Build a functioning robot for once.'
-var $textBox = $('.intro')
-console.log($textBox)
+var word = 'Mission: Build a functioning robot for once.';
+var $textBox = $('.intro');
+console.log($textBox);
 
 const typeWord = () => {
-    let cur = word.charAt(i)
-    i += 1
-    $textBox.text($textBox.text() + cur)
-    if(i < word.length) setTimeout(typeWord, 30)
+    let cur = word.charAt(i);
+    i += 1;
+    $textBox.text($textBox.text() + cur);
+    if(i < word.length) setTimeout(typeWord, 30);
 }
 
-setTimeout(typeWord, 500)
+setTimeout(typeWord, 500);
