56537253836663f106f8782e5a35ff467bcb2e7a
Justin Rivera <50186921+jriv01@users.noreply.github.com>
2023-10-30 19:52:50 -0400

    Update lead info

--- populate_team.js
+++ populate_team.js
@@ -28,7 +28,7 @@ const getLeadLinks = lead => {
         html += `<a target="_blank" href="${lead.instagram}"><img src="./resources/icons/insta-icon.png" class="link-icon"></a>`
     }
     if(lead.website != null) {
-        html += `<a target="_blank" href="${lead.website}"><img src="./resources/icons/desktop-icon.png" class="link-icon"></a>`
+        html += `<a target="_blank" href="${lead.website}"><img src="./resources/icons/www-icon.png" class="link-icon"></a>`
     }
 
     // Close div and return
